c6dab8c4a1f4c7b636171432d652c1ffe4e5b00a
frausto1106 <frausto.isunza.emmanuelkaled@gmail.com>
2023-09-05 17:58:35 -0600

    cifrado pro pero con poo 😮‍💨

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,6 @@ import java.io.FileReader;
 import org.json.JSONTokener;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.security.Key;
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
@@ -17,6 +16,7 @@ public class Main {
     private static final String LLAVE = "byronbyronbyron1";
 
     public static void main(String[] args) {
+        func cifrado = new func();
         Scanner leer = new Scanner(System.in);
 
         while (true) {
@@ -30,9 +30,9 @@ public class Main {
             opc = leer.next();
 
             if (opc.equals("1")) {
-                escribir(leer);
+                cifrado.escribir();
             } else if (opc.equals("2")) {
-                leer();
+                cifrado.leer();
             } else if (opc.equals("3")) {
                 leer.close();
                 break;
@@ -42,52 +42,6 @@ public class Main {
         }
     }
 
-    private static void escribir(Scanner leer) {
-        leer.nextLine(); // Limpia el buffer del Scanner
-        System.out.print("Ingresa el texto: ");
-        String input = leer.nextLine();
 
-        try {
-            SecretKey key = new SecretKeySpec(LLAVE.getBytes(StandardCharsets.UTF_8), "AES");
 
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-            byte[] cifrado = cipher.doFinal(input.getBytes(StandardCharsets.UTF_8));
-
-            String cifradoBase64 = Base64.getEncoder().encodeToString(cifrado);
-
-            JSONObject msj = new JSONObject();
-            msj.put("mensaje_cifrado", cifradoBase64);
-
-            try (FileWriter file = new FileWriter("mensaje.json")) {
-                file.write(msj.toString());
-                System.out.println("Archivo JSON creado exitosamente en la carpeta del proyecto :).");
-            } catch (IOException e) {
-                System.out.println("Ocurrió un error al crear el archivo JSON: " + e.getMessage());
-            }
-        } catch (Exception e) {
-            System.out.println("Ocurrió un error al cifrar el mensaje: " + e.getMessage());
-        }
-    }
-
-    private static void leer() {
-        try {
-            FileReader file = new FileReader("mensaje.json");
-            JSONObject msj = new JSONObject(new JSONTokener(file));
-            String cifradoBase64 = msj.getString("mensaje_cifrado");
-            byte[] cifrado = Base64.getDecoder().decode(cifradoBase64);
-
-            SecretKey key = new SecretKeySpec(LLAVE.getBytes(StandardCharsets.UTF_8), "AES");
-
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.DECRYPT_MODE, key);
-            byte[] descifrado = cipher.doFinal(cifrado);
-
-            System.out.println("El mensaje descifrado es: " + new String(descifrado, StandardCharsets.UTF_8));
-        } catch (IOException e) {
-            System.out.println("Ocurrió un error al leer el archivo JSON: " + e.getMessage());
-        } catch (Exception e) {
-            System.out.println("Ocurrió un error al descifrar el mensaje: " + e.getMessage());
-        }
-    }
 }
